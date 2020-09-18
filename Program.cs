924a36fb24bce9638dba393200bab235da3da6f7
Jimmy <jimmy123good@hotmail.com>
2020-09-18 14:55:41 -0700

    set up

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _499w2
+{
+    interface BinaryOperator<T> {
+        T call(T a, T b);
+    }
+
+    class Add<T> : BinaryOperator<T> {
+        public T call(T a, T b) {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return (T)(a1 + b1);
+        }
+    }
+
+    class Minus<T> : BinaryOperator<T> {
+        public T call(T a, T b) {
+            throw new NotImplementedException("Fill it here");
+        }
+    }
+
+    class Muliply<T> : BinaryOperator<T> {
+        public T call(T a, T b) {
+            throw new NotImplementedException("Fill it here");
+        }
+    }
+
+    class Divide<T> : BinaryOperator<T> {
+        public T call(T a, T b) {
+            throw new NotImplementedException("Fill it here");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var addOp = new Add<int>();
+            // add test;
+
+            Console.WriteLine("My add works: " + addOp.call(1, 2));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
