599f3857cc9782851145eb76104275cd5aec72c0
tm.1998 <tm.1998@163.com>
2020-09-19 16:04:22 -0700

    minus

--- Program.cs
+++ Program.cs
@@ -16,7 +16,9 @@ namespace _499w2
 
     class Minus<T> : BinaryOperator<T> {
         public T call(T a, T b) {
-            throw new NotImplementedException("Fill it here");
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return (T)(a1 - b1);
         }
     }
 
@@ -37,9 +39,11 @@ namespace _499w2
         static void Main(string[] args)
         {
             var addOp = new Add<int>();
+            var minOp = new Minus<int>();
             // add test;
 
             Console.WriteLine("My add works: " + addOp.call(1, 2));
+            Console.WriteLine("My minus(Giles) works: " + minOp.call(4, 2));
             Console.WriteLine("Hello World!");
         }
     }
