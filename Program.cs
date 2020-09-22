c52a6ff9c6f30163d2e6f6f6e01c19936a9bf7f9
wintro <manassawinrotsawatsuk@gmail.com>
2020-09-22 12:20:01 -0700

    Implemented Divide & its test.

--- Program.cs
+++ Program.cs
@@ -30,7 +30,9 @@ namespace _499w2
 
     class Divide<T> : BinaryOperator<T> {
         public T call(T a, T b) {
-            throw new NotImplementedException("Fill it here");
+            dynamic a1 = a;
+            dynamic b1 = b;
+            return (T)(a1 / b1);
         }
     }
 
@@ -40,10 +42,12 @@ namespace _499w2
         {
             var addOp = new Add<int>();
             var minOp = new Minus<int>();
+            var divOp = new Divide<int>();
             // add test;
 
             Console.WriteLine("My add(Jimmy) works: " + addOp.call(1, 2));
             Console.WriteLine("My minus(Giles) works: " + minOp.call(4, 2));
+            Console.WriteLine("My divide(Winter) works: " + divOp.call(16, 2));
             Console.WriteLine("Hello World!");
         }
     }
